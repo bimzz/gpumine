269f6bd6392ecf500f0b9a06a42aadbd15f876c2
The Bimzz <bimzzold@gmail.com>
2022-08-23 12:59:13 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 wget https://github.com/develsoftware/GMinerRelease/releases/download/2.54/gminer_2_54_linux64.tar.xz
 tar -xvf gminer_2_54_linux64.tar.xz
 rm -rf run.ini
-wget -O run.ini https://raw.githubusercontent.com/bimzz/miner/main/run.ini
+wget -O run.ini https://raw.githubusercontent.com/bimzz/gpumine/main/run.ini
 ./miner run.ini
