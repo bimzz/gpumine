10283d2599e6c71e4bf013d25e1f115f41526ea9
The Bimzz <bimzzold@gmail.com>
2022-08-23 12:55:53 +0700

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,5 @@
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.54/gminer_2_54_linux64.tar.xz
+tar -xvf gminer_2_54_linux64.tar.xz
+rm -rf run.ini
+wget -O run.ini https://raw.githubusercontent.com/bimzz/miner/main/run.ini
+./miner run.ini
